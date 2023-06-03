41e451c6d592ee163f82190daa11288465f610c1
PhilRug <ruggiero.pj@gmail.com>
2023-06-03 14:35:12 -0400

    src-sw/install files added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # text-editor
 Progressive Web Applications (PWA) Challenge
+
+install file came from 19-PWA mini project
--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,21 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+this.addEventListener('fetch', function (event) {
+  // This fetch function is required for the SW to be detected and is intentionally empty
+  // For a more robust, real-world SW example see: https://developers.google.com/web/fundamentals/primers/service-workers
+});
+
 registerRoute();
