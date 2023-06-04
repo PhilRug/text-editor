9266eb26c5d45bd1242cdd0d274d5ad6148c14e0
PhilRug <ruggiero.pj@gmail.com>
2023-06-04 18:43:01 -0400

    scripts and css plugin add

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -23,7 +24,7 @@ module.exports = () => {
         template: './index.html',
         title: 'Contact Cards'
       }),
-     
+      new MiniCssExtractPlugin(),
       // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
@@ -55,7 +56,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.m?js$/,
